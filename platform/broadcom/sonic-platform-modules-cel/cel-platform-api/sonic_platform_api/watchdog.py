f83d2a7c57df310222eaf0ceecfc1d519c449bad
pleamsum <pleamsum@celestica.com>
2019-02-14 14:10:42 +0700

    Fixed watchdog did not stop issue

--- platform/broadcom/sonic-platform-modules-cel/cel-platform-api/sonic_platform_api/watchdog.py
+++ platform/broadcom/sonic-platform-modules-cel/cel-platform-api/sonic_platform_api/watchdog.py
@@ -9,7 +9,8 @@
 #############################################################################
 import ctypes
 import fcntl
-from ioctl_opt import IOC, IOC_READ, IOR
+import os
+from ioctl_opt import IOC, IOC_READ, IOR, IOWR
 import sonic_platform
 
 try:
@@ -17,39 +18,92 @@ try:
 except ImportError as e:
 	raise ImportError(str(e) + "- required module not found")
 
+WATCHDOG_IOCTL_BASE = ord('W')
+int_size = ctypes.c_int
+
+# WDIOC_GETSUPPORT = IOR(WATCHDOG_IOCTL_BASE, 0, struct_watchdog_info_size)
+WDIOC_GETSTATUS = IOR(WATCHDOG_IOCTL_BASE, 1, int_size)
+WDIOC_GETBOOTSTATUS = IOR(WATCHDOG_IOCTL_BASE, 2, int_size)
+WDIOC_GETTEMP = IOR(WATCHDOG_IOCTL_BASE, 3, int_size)
+WDIOC_SETOPTIONS = IOR(WATCHDOG_IOCTL_BASE, 4, int_size)
+WDIOC_KEEPALIVE = IOR(WATCHDOG_IOCTL_BASE, 5, int_size)
+WDIOC_SETTIMEOUT = IOWR(WATCHDOG_IOCTL_BASE, 6, int_size)
+WDIOC_GETTIMEOUT = IOR(WATCHDOG_IOCTL_BASE, 7, int_size)
+WDIOC_SETPRETIMEOUT = IOWR(WATCHDOG_IOCTL_BASE, 8, int_size)
+WDIOC_GETPRETIMEOUT = IOR(WATCHDOG_IOCTL_BASE, 9, int_size)
+WDIOC_GETTIMELEFT = IOR(WATCHDOG_IOCTL_BASE, 10, int_size)
+
+WDIOS = {
+	"DISABLECARD": "1",    # Turn off the watchdog timer
+	"ENABLECARD": "2",     # Turn on the watchdog timer
+	"TEMPPANIC": "4",      # Kernel panic on temperature trip
+}
+
 class Watchdog(WatchdogBase):
 
 	def __init__(self):
-
-		self.watchdog_path = "/dev/watchdog1"
+		self.device_path = "/dev/watchdog1"
 		self.watchdog_status_path = "/sys/class/watchdog/watchdog1/status"
 		self.watchdog_state_path = "/sys/class/watchdog/watchdog1/state"
 
-		self.WDIOS_DISABLECARD = "1"
-		self.WDIOS_ENABLECARD = "2"
+	@property
+	def is_running(self):
+		return self._fd is not None
 
-		self.WDIOC_SETOPTIONS = IOR(ord('W'), 4, ctypes.c_int)
-		self.WDIOC_KEEPALIVE = IOR(ord('W'), 5, ctypes.c_int)
+	def open(self):
+		try:
+			self._fd = os.open(self.device_path, os.O_WRONLY)
+		except OSError as e:
+			raise OSError("Watchdog error({0}): {1}".format(e.errno, e.strerror))
+
+	def close(self):
+		if self.is_running:
+			try:
+				os.write(self._fd, b'V')
+				os.close(self._fd)
+				self._fd = None
+			except OSError as e:
+				raise OSError("Watchdog error({0}): {1}".format(e.errno, e.strerror))
+
+	def _ioctl(self, func, arg):
+		"""Runs the specified ioctl on the underlying fd.
+
+		Raises WatchdogError if the device is closed.
+		Raises OSError or IOError (Python 2) when the ioctl fails."""
+		if self._fd is None:
+			raise IOError("Watchdog error({0}): {1}".format(e.errno, e.strerror))
+		fcntl.ioctl(self._fd, func, arg)
+
+	def keepalive(self):
+		if self._fd is None:
+			raise IOError("Watchdog device is closed")
+		try:
+			os.write(self._fd, b'1')
+		except OSError as e:
+			raise OSError("Watchdog error({0}): {1}".format(e.errno, e.strerror))
+
+	#################################################################
 
 	def arm(self, seconds):
 		## TODO : Not implement timeout setting.
 		self.seconds = seconds
-
 		try:
-			with open(self.watchdog_path, "w") as fd:
-				ret = fcntl.ioctl(fd, self.WDIOC_SETOPTIONS, self.WDIOS_ENABLECARD)
-		except IOError as e:
-		    print "I/O error({0}): {1}".format(e.errno, e.strerror)
-		    raise IOError("Unable to send ioctl !")
+			self.open()
+			self.keepalive()
+			self.close()
+		except (OSError, IOError) as e:
+		    raise IOError("Watchdog error({0}): {1}".format(e.errno, e.strerror))
+		
 		return True
 
 	def disarm(self):
+		print "WDIOC_SETOPTIONS ",WDIOC_SETOPTIONS
 		try:
-			with open(self.watchdog_path, "w") as fd:
-				ret = fcntl.ioctl(fd, self.WDIOC_SETOPTIONS, self.WDIOS_DISABLECARD)
-		except IOError as e:
-		    print "I/O error({0}): {1}".format(e.errno, e.strerror)
-		    raise IOError("Unable to send ioctl !")
+			self.open()
+			self._ioctl(WDIOC_SETOPTIONS, WDIOS['DISABLECARD'])
+			self.close()
+		except (OSError, IOError) as e:
+		    raise IOError("Watchdog error({0}): {1}".format(e.errno, e.strerror))
 		return True;
 
 	def is_arm(self):
@@ -59,9 +113,8 @@ class Watchdog(WatchdogBase):
 					return True
 				else:
 					return False
-		except IOError as e:
-			print "I/O error({0}): {1}".format(e.errno, e.strerror)
-			raise IOError("Unable to send ioctl !")
+		except (OSError, IOError) as e:
+			raise IOError("Watchdog error({0}): {1}".format(e.errno, e.strerror))
 
 
 	def get_remaining_time(self):
